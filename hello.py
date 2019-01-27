f6ffaf371e9cec3dcac1c18b74cf22d31ff66831
bhagvank <bhagvan.kommadi@apples-MacBook-Air.local>
2019-01-27 23:34:41 +0530

    added hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+import numpy as np
+import cv2
+
+image = cv2.imread('Contours.jpg')
+imagegray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+ret,threshold = cv2.threshold(imagegray,127,255,0)
+image, contours, hierarchy = cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+print("contours",contours)
