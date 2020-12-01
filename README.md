42195c487b41b3931d2bb9277ccff3f086fe0738
bhagvank <bhagvan.kommadi@apples-MacBook-Air.local>
2020-12-01 13:06:06 +0530

    added image analysis

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # computer_vision
 computervision, image processing and machine learning
+
+
+# Finding Contours
+python3 hello.py
+
+
+# Reading Threshold, Gray, Finding objects
+
+python3 image_analysis.py -i testris.jpeg
--- hello.py
+++ hello.py
@@ -4,5 +4,5 @@ import cv2
 image = cv2.imread('Contours.jpg')
 imagegray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 ret,threshold = cv2.threshold(imagegray,127,255,0)
-image, contours, hierarchy = cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 print("contours",contours)
